afec9dc0d1a2f2890dbca314e2efe1fb0ad37499
BimoFr <bimofadhlurrahman@gmail.com>
2025-05-08 10:33:06 +0700

    Memperbaiki kesalahan sintaks, dan menambahkan pernyataan untuk mengecek nim untuk mencegah duplikasi nim

--- file1.cpp
+++ file1.cpp
@@ -19,14 +19,25 @@ public:
     {
         START = NULL;
     }
+
+    void addnote()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa: ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if ((START != NULL) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+        }
+        Node *previous = START;
+        Node *current = START;
+    }
 };
-
-void addnote()
-{
-    int nim;
-    cout << "\nMasukkan Nomor Mahasiswa: ";
-    cin >> nim;
-
-    Node *nodeBaru = new Node;
-    nodeBaru->noMhs = nim;
-}
